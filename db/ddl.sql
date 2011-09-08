7f82e012eaf93877d6e1faa9f5088c17b0223c81
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-09-09 01:23:30 +0900

    add index

--- db/ddl.sql
+++ db/ddl.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS sc_tag_view_logs (
 	post_id INTEGER NULL,
 	logged_at DATETIME NOT NULL,
 	PRIMARY KEY(id)
-);
+) DEFAULT CHARACTER SET UTF8;
 
 CREATE TABLE IF NOT EXISTS sc_tag_rankings (
 	rank INT NOT NULL,
@@ -15,7 +15,11 @@ CREATE TABLE IF NOT EXISTS sc_tag_rankings (
 	score INT NOT NULL,
 	status VARCHAR(16) NOT NULL,
 	rankinged_at DATETIME NOT NULL,
-	update_at DATETIME NOT NULL
-);
+	updated_at DATETIME NOT NULL
+) DEFAULT CHARACTER SET UTF8;
+
+CREATE INDEX sc_tag_view_logs_logged_at ON sc_tag_view_logs (logged_at);
+CREATE INDEX sc_tag_rankings_status_rankinged_at ON sc_tag_rankings (status, rankinged_at);
+CREATE INDEX sc_tag_rankings_status_score ON sc_tag_rankings (status, score);
 
 COMMIT;
