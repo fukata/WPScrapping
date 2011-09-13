206044496b225199243d1f922dfae955a64406e0
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-09-14 04:50:45 +0900

    remove fb

--- wordpress/plugins/scrapping/themes/default/single.php
+++ wordpress/plugins/scrapping/themes/default/single.php
@@ -17,15 +17,6 @@
 	<a href="<?=Scrap::get_meta_url($post->ID)?>" title="<?=htmlspecialchars(Scrap::get_meta_title($post->ID))?>" rel="nofollow"><img src="<?=Scrap::get_thumbnail_url($post->ID, 'large')?>"/></a>
 
 	<p><?=Scrap::get_description($post)?></p>
-
-<div id="fb-root"></div>
-<script>(function(d){
-  var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-  js = d.createElement('script'); js.id = id; js.async = true;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  d.getElementsByTagName('head')[0].appendChild(js);
-}(document));</script>
-<div class="fb-comments" data-href="wp3.org" data-num-posts="10"></div>
 </div>
 
 <div data-role="footer">
