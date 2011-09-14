ec488822dbadb0c030e506ec9e22a401dd2e8977
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-09-15 01:21:12 +0900

    ナビゲーションバーを上下に追加

--- wordpress/plugins/scrapping/themes/default/single.php
+++ wordpress/plugins/scrapping/themes/default/single.php
@@ -10,11 +10,28 @@
 </head>
 <body>
 <div data-role="page">
+	<?php $prev_post = get_adjacent_post(true, '', true); ?>
+	<?php $next_post = get_adjacent_post(true, '', false); ?>
 
 	<div data-role="header" data-position="inline">
 		<a href="<?=home_url( '/' )?>" data-icon="home">ホーム</a>
 		<h1><?=Scrap::get_meta_title($post->ID)?></h1>
 	</div>
+
+	<div data-role="navbar">
+		<ul>
+			<li>
+			<?php if ( $prev_post ) { ?>
+			<a href="<?=get_permalink($prev_post)?>" title="<?=get_the_title($prev_post->ID)?>" data-icon="arrow-l"><?=Scrap::get_short_title($prev_post->ID)?></a>
+			<?php } ?>
+			</li>
+			<li>
+			<?php if ( $next_post ) { ?>
+			<a href="<?=get_permalink($next_post)?>" title="<?=get_the_title($next_post->ID)?>" data-icon="arrow-r"><?=Scrap::get_short_title($next_post->ID)?></a>
+			<?php } ?>
+			</li>
+		</ul>
+	</div><!-- /navbar -->
 
 	<div data-role="content">
 		<p style="text-align:center;"><a href="<?=Scrap::get_meta_url($post->ID)?>"><?=Scrap::get_meta_title($post->ID)?></a></p>
@@ -22,6 +39,21 @@
 
 		<p><?=Scrap::get_description($post)?></p>
 	</div>
+
+	<div data-role="navbar">
+		<ul>
+			<li>
+			<?php if ( $prev_post ) { ?>
+			<a href="<?=get_permalink($prev_post)?>" title="<?=get_the_title($prev_post->ID)?>" data-icon="arrow-l"><?=Scrap::get_short_title($prev_post->ID)?></a>
+			<?php } ?>
+			</li>
+			<li>
+			<?php if ( $next_post ) { ?>
+			<a href="<?=get_permalink($next_post)?>" title="<?=get_the_title($next_post->ID)?>" data-icon="arrow-r"><?=Scrap::get_short_title($next_post->ID)?></a>
+			<?php } ?>
+			</li>
+		</ul>
+	</div><!-- /navbar -->
 
 	<?php include(dirname(__FILE__).'/footer.php'); ?>
 </div>
